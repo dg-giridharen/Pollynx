028ed2d42637c458e85d3b1085937e5cfcda4ce0
dg-giridharen <dg.giridharen@gmail.com>
2026-08-17 14:47:45 +0530

    feat: added domain entities

---
+++ Pollynx.Domain/Entities/PollOption.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pollynx.Domain.Entities;
+
+public class PollOption
+{
+    public int Id { get; set; }
+
+    public int PollId { get; set; }
+
+    public string OptionText { get; set; } = string.Empty;
+
+    public Poll Poll { get; set; } = null!;
+
+    public ICollection<Vote> Votes { get; set; } = new List<Vote>();
+}
