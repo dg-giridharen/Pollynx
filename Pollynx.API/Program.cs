61f9e8b7ac2447f6fbd00fd752d0f27d603dbf5f
dg-giridharen <dg.giridharen@gmail.com>
2026-08-20 11:32:41 +0530

    feat: seed users and configure RBAC verification

--- Pollynx.API/Program.cs
+++ Pollynx.API/Program.cs
@@ -80,6 +80,14 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider
+        .GetRequiredService<ApplicationDbContext>();
+
+    await DbSeeder.SeedAsync(context);
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
